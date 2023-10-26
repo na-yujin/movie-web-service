cf83c5792f243e1eb219b82a80c18972203f497a
na-yujin <kfoyujin@gmail.com>
2023-10-26 21:20:35 +0900

    feat: useEffect test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import Button from "./Button";
-import styled from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1)
+
+  console.log("i run all the time");
+  // TODO : useEffect는 두개의 argument를 가지는 function이다.
+  /*
+    첫번째 argument는 우리가 딱 한번만 실행하고 싶은 코드
+    두번째 argument는..
+  */
+
+  useEffect(() => {
+    console.log("CALL THE API...")
+  }, []);
+
   return (
     <div>
-      <h1 className={styled.title}>버튼</h1>
-      <Button text={"true"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
