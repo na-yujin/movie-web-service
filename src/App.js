ad8f836c4a7608fea1b9f3e54dd94a20a760cd11
na-yujin <kfoyujin@gmail.com>
2023-10-27 09:57:33 +0900

    feat: useEffect 의존성배열

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1)
   const onChange = (event) => setKeyword(event.target.value);
-
+  // TODO - 231027
+  /*
+    react특징 : component를 새로운 데이터가 들어올때다가 UI를 refresh(새로고침)한다.
+    useEffects는 두개의 argument를 가지는 function이다.
+    첫번째 argument는 내가 실행시키고 싶은 코드
+    두번째 dependency는 react가 지켜보고 그것이 변화할 때 react가 코드 실행시킴
+   */
   useEffect(() => {
     console.log("i run only once");
   }, []);
@@ -13,7 +19,7 @@ function App() {
     console.log("i run when 'keyword' changes");
   }, [keyword]);
   useEffect(() => {
-      console.log("i run when 'counter' changes");
+    console.log("i run when 'counter' changes");
   }, [counter]);
   useEffect(() => {
     console.log("i run when keyword & counter change")
