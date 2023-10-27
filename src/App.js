9e95e2ee1ddf6b31bebea0835a5574a372807e89
na-yujin <kfoyujin@gmail.com>
2023-10-27 18:06:10 +0900

    feat: React Router ~

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 //TODO : 231027
 /*
   component를 쪼개서 Home.js에서 Movie.js에게 prop전달할 수 있게 만들었다.
 */
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
