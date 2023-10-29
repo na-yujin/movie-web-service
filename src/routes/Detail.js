58f1bd63e16badbc954c7b023f8a585a728ceb9e
‘na-yujin’ <90164093+nayujina@users.noreply.github.com>
2023-10-29 22:26:57 +0900

    feat: Parameters

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,38 @@
+import { useParams } from "react-router-dom";
+import {useEffect, useState} from "react";
+import Movie from "../components/Movie";
 function Detail() {
-  return <h1>Detail</h1>
+  const {id} = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/list_movies.json?movie_id=${id}`)
+    ).json();
+    console.log(json)
+    setMovies(json.data.movies);
+    setLoading(false)
+  }
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (<h1>Loading</h1>) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 export default Detail;
